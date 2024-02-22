3ddb41ca1e5e4b3a8dcc04f70e475e87e2134432
GevS <gevs.sargsyan96@gmail.com>
2024-02-22 18:37:55 +0400

    add todowhitUseReducer

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import BasicCounter from './components/basicCounter/basicCounter'
 import ToDoList from './components/todoList/todoList'
+import ToDoListWithReducer from './components/todoListWithReducer/todoList/todoListWithReducer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <BasicCounter/> */}
-    <ToDoList/>
+    {/* <ToDoList/> */}
+    <ToDoListWithReducer />
   </React.StrictMode>,
 )
